088238f58b97a88b19ad1354cc4c198778dc2be7
chandrarma <chandrarma@ecs.vuw.ac.nz>
2019-10-27 14:07:06 +1300

    trying to connect to a database

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: "postgres://oryzomobpohgzz:a103104857c587015cf3389470f9cb436c8d652d0dc5fc20b759cb7ff94c133b@ec2-54-83-33-14.compute-1.amazonaws.com:5432/ddee184a8ugadt",
@@ -13,12 +12,12 @@ express()
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
-  .get('/db', async (req, res) => {
+  .get('/data', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM user_accounts');
+      const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('viwes/pages/db', results ); 
+      res.render('db', results );
       client.release();
     } catch (err) {
       console.error(err);
